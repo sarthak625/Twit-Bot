3cf76a5afc5b2d2abf0882fb817ff9de32653509
Sarthak <sarthakn18@gmail.com>
2017-06-17 05:41:16 +0530

    Added config and modules

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports={
+	consumer_key: 'mH7K78RJwtjLlQdCsNzpnkGVA',
+	consumer_secret: 'IwFrdoC5PzS2MNbQOVAkP8ORqO7Uhor2aPjBzyxV7ysrghxkkq',
+	access_token: '875710353635770369-p7gVXwDvdUlqjKG1fAf45j26WdiCfLo',
+	access_token_secret: 'A7EIi9bVeurBfXutuK9AUXN4YiPhZlHIk4oaDGzEl1HJu'
+}
