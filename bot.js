3503fb4bf0a8fd9a26fc8e1912856de69c1a93b9
Sarthak <sarthakn18@gmail.com>
2017-06-17 06:08:16 +0530

    Added capability to tweet and search tweets

--- bot.js
+++ bot.js
@@ -5,32 +5,40 @@ var config = require('./config');
 
 var T = new Twit(config);
 
-var params = {
-	q: 'Man Utd',
-	count: 2
-}
-
+//Code to search for a tweet
+function searchIt(searchString, tweetCount){
 T.get('search/tweets', params, gotData);
+	
+	var params = {
+		q: searchString,
+		count: tweetCount
+	}
 
-function gotData(err,data,response){
-	var tweets = data.statuses;
-	for (var i=0;i<tweets.length;i++){
-		console.log(tweets[i].text);
+	function gotData(err,data,response){
+		var tweets = data.statuses;
+		for (var i=0;i<tweets.length;i++){
+			console.log(tweets[i].text);
+		}
 	}
 }
 
-var tweet = {
-	status: 'Life is awesome'
+//Code to post a tweet
+function tweetIt(update){
+	var tweet = {
+		status: update
+	}
+
+
+
+	T.post('statuses/update', tweet, tweeted);
+
+	function tweeted(err,data,response){
+		if (err)
+			console.log("Something went wrong");
+		else
+			console.log("Tweet");
+	}
 }
 
-T.post('statuses/update', tweet, tweeted);
-
-function tweeted(err,data,response){
-	if (err)
-		console.log("Something went wrong");
-	else
-		console.log("Tweet");
-}
-
-
+tweetIt("Node bot worrrkkkiinnngggg............");
 
